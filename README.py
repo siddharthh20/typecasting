f8c42ead26e80f6f920cd0235c1a417ed7a4acf0
Siddharth Sigh <83107315+sidsomya12@users.noreply.github.com>
2021-06-22 12:18:48 +0530

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,12 @@
+# Typecasting in Python :
+abc = 5 
+abc2 = '45'
+abc3 = 55.95
+xyz = 5.0
+
+abc4=int(abc2)
+
+print(abc+abc4) # Output : 50 
+print(abc+int(abc2)) # Output : 50 
+
+print(float(abc)+xyz) # It will add 5.0 + 5.0 and will return 10.0
